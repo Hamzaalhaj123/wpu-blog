61ae635b891b643feeda7765222ff42867e4013d
Mohamad Taky <mohamadtakydev@gmail.com>
2024-08-23 09:28:34 +0300

    resolved responsive blogsFeed grid cols

--- src/app/_components/home/BlogsFeed.tsx
+++ src/app/_components/home/BlogsFeed.tsx
@@ -3,7 +3,7 @@ import { Blog } from ".";
 
 export default function BlogsFeed() {
   return (
-    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
+    <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
       {blogs.map((blog) => (
         <Blog key={blog.id} {...blog} />
       ))}
