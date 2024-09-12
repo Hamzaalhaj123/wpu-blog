48bd3b66656196f97867485197817b1f738fb16f
Mohamad Taky <mohamadtakydev@gmail.com>
2024-09-12 21:20:35 +0300

    Revert "updated messages.en"
    This reverts commit 3543d683d7c8ca1d02d6c35a95460bbfacee4884.

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  return NextResponse.json({ message: "Hello world123" });
+  return NextResponse.json({ message: "Hello world" });
 }
