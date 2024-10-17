21652ca1326d6855172da79f54883556e7064a9d
Mohamad Taky <mohamadtakydev@gmail.com>
2024-10-17 21:42:19 +0300

    resolved build errors

--- src/app/[locale]/auth/page.tsx
+++ src/app/[locale]/auth/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import LoginForm from "@/components/auth/LoginForm";
+import SignInForm from "@/components/auth/SignInForm";
 import SignUpForm from "@/components/auth/SignUpForm";
 import Button from "@/components/shared/Button";
 import { useState } from "react";
@@ -9,14 +9,10 @@ export default function Page() {
 
   return (
     <div className="flex h-full items-center justify-center">
-      <div className="mx-auto flex w-full max-w-xl flex-col gap-6 rounded-xl bg-background-lighter p-6">
-        {method === "login" ? <LoginForm /> : <SignUpForm />}
+      <div className="bg-background-lighter mx-auto flex w-full max-w-xl flex-col gap-6 rounded-xl p-6">
+        {method === "login" ? <SignInForm /> : <SignUpForm />}
         <div className="flex items-center gap-2">
-          <p>
-            {method === "login"
-              ? "Don't have an account yet?"
-              : "Already have an account?"}
-          </p>
+          <p>{method === "login" ? "Don't have an account yet?" : "Already have an account?"}</p>
 
           <Button
             // variant={"link"}
