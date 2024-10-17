88aa4fb1f9baa537ef3d0f4f34e60d367c44d6d4
Hamzaalhaj123 <hamzaalhaj987@gmail.com>
2024-10-17 23:04:39 +0300

    Added signup logout button

--- src/components/auth/Logout.tsx
+++ src/components/auth/Logout.tsx
@@ -2,9 +2,17 @@
 import { logout } from "@/actions/auth/logout";
 import Button from "@/components/shared/Button";
 import { useSession } from "@/components/wrappers/SessionProvider";
+import routes from "@/config/routes";
+import { Link } from "@/lib/next-intl/navigation";
 
 export default function Logout() {
   const { session } = useSession();
 
-  return session ? <Button onClick={() => logout()}>Logout</Button> : null;
+  return session ? (
+    <Button onClick={() => logout()}>Logout</Button>
+  ) : (
+    <Button asChild>
+      <Link href={routes.signUp}>Sign up</Link>
+    </Button>
+  );
 }
